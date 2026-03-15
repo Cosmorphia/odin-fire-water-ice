c81f5abc7e65a8f75d34d5ffcbf2a666545b9156
Cosmorphia <179248894+Cosmorphia@users.noreply.github.com>
2026-03-15 03:04:26 -0400

    Add compareInputs function

--- game.js
+++ game.js
@@ -20,3 +20,28 @@ function getComputerInput() {
         return computerInput = "ice";
     }
 }
+
+// compare both inputs and return result
+function compareInputs() {
+    // invalid input condition
+    if ((humanInput !== "fire")
+    && (humanInput !== "water")
+    && (humanInput !== "ice")) {
+        return winCondition = "invalid";
+    }
+    // tie condition
+    else if (humanInput === computerInput) {
+        return winCondition = "tie";
+    }
+    // human win condition
+    else if ((humanInput === "fire" && computerInput === "ice")
+    || (humanInput === "water" && computerInput === "fire")
+    || (humanInput === "ice" && computerInput === "water")) {
+        return winCondition = "human";
+    }
+    // computer win condition
+    else {
+        return winCondition = "computer";
+    }
+
+}
